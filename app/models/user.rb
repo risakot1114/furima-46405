df74e7ec7873308fbdcd61df40c717ed465b578f
k14is047keisenacjp-ship-it <k14is047keisen.ac.jp@gmail.com>
2025-09-29 16:08:57 +0900

    トップページ遷移

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # パスワードの半角英数字混合チェック
+  validates :password, format: {
+    with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/,
+    message: 'は半角英字と数字の両方を含めてください'
+  }, if: -> { password.present? }
+
+  # パスワード確認との一致はDeviseが自動でやってくれる
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_26_070032) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_29_070213) do
   create_table "articles", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_26_070032) do
     t.date "birthday", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
