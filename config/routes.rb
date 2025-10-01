5fe2169d9cd2b6400c7949dfc591649241e75975
k14is047keisenacjp-ship-it <k14is047keisen.ac.jp@gmail.com>
2025-10-01 14:49:33 +0900

    出品コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
 
-  # healthチェックはそのまま残す
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # ルートをitems#indexに設定
   root "items#index"
-
-  # items用のルーティング
   resources :items
 
    # ユーザー詳細ページのルーティング（ヘッダー用）
