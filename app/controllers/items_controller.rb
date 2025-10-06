71329328ee00bb77b92535b638396f676dc46a8a
k14is047keisenacjp-ship-it <k14is047keisen.ac.jp@gmail.com>
2025-10-06 18:20:44 +0900

    Delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:edit, :update, :show]
-  before_action :move_to_root_unless_owner, only: [:edit, :update]
+  before_action :move_to_root_unless_owner, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -32,6 +32,11 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
