26f626ca8065aa13049c4c530b2112798a5854a1
k14is047keisenacjp-ship-it <k14is047keisen.ac.jp@gmail.com>
2025-10-15 15:11:12 +0900

    JS変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,19 +4,26 @@ class OrderAddress
                 :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'must be in the format XXX-XXXX' }
-    validates :prefecture_id, numericality: { other_than: 0, message: 'must be other than 0' }
-    validates :city
-    validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'must be 10-11 digits, numbers only' }
+    validates :token, presence: { message: "can't be blank" }
+
+    validates :postal_code, presence: { message: "can't be blank" },
+                            format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+    validates :city, presence: { message: "can't be blank" }
+    validates :address, presence: { message: "can't be blank" }
+
+    validates :phone_number, presence: { message: "can't be blank" },
+                             format: { with: /\A\d+\z/, message: 'is invalid. Input only number' },
+                             length: { minimum: 10, maximum: 11, too_short: 'is too short' }
+
     validates :user_id
     validates :item_id
-    validates :token
   end
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-
     Address.create(
       postal_code: postal_code,
       prefecture_id: prefecture_id,
