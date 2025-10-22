b5bb0eee0f3727e55c89558e1e7ab88c3bdb7734
k14is047keisenacjp-ship-it <k14is047keisen.ac.jp@gmail.com>
2025-10-22 11:13:12 +0900

    apppath変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ worker_processes 1
 working_directory app_path
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{File.expand_path('../../', __FILE__)}/tmp/pids/unicorn.pid"
+pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen "#{app_path}/tmp/sockets/unicorn.sock"
@@ -20,7 +20,7 @@ stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
-timeout 120
+timeout 60
 
 #以下は応用的な設定なので説明は割愛
 
